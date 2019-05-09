578c098a268ad0b300e5db1d0d337588297cba1a
Anderson Sanches <andycds@gamail.com>
2019-05-08 21:51:04 -0300

    NotableNotes3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NoteService } from '../../app/note.service';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  notes;
 
+  constructor(public navCtrl: NavController, private noteService: NoteService) {
+    this.notes = noteService.notes;
   }
+
+  onItemClick(note) {
+    //console.log("item-click", note)
+    this.navCtrl.push('DetailPage', {
+      noteParam: note
+    });
+  }
+/*
+  notes = [
+    {
+      id: '1',
+      date: '2016-02-01',
+      title: 'Firebase',
+      content: 'Que tal programar Serverless?'
+    },
+    {
+      id: '2',
+      date: '2016-01-01',
+      title: 'Ionic',
+      content: 'Aprenda o básico de Ionic'
+    },
+    {
+      id: '3',
+      date: '2016-03-01',
+      title: 'Angular',
+      content: 'Importante para desenvolver com Ionic'
+    }
+  ]*/
 
 }
